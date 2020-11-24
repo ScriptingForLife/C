52eba35d1086ea6c265131da7721b1292c60fe48
Methyl <73468685+ScriptingForLife@users.noreply.github.com>
2020-11-25 00:36:35 +0100

    New function used

--- includes/puts.h
+++ includes/puts.h
@@ -1,1 +1,30 @@
+// # Methyl
+// # Language used : C 
+// # Filename : puts.h
 
+// # E-mail : methyls3c@gmail.com
+// # Site Web : https://methylean.space
+// # Github : https://github.com//ScriptingForLife
+
+
+void my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+int my_putint(int i)
+{
+  char c;
+  c = (char) i;
+  my_putchar(c);
+  return 0;
+}
+
+void my_putstr(char *str)
+{
+  while(*str != 0)
+  {
+    my_putchar(*str);
+    str++;
+  }
+}
