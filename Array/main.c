050549c7125ec863fcb1751373547e5550e11434
Methyl <73468685+ScriptingForLife@users.noreply.github.com>
2020-11-25 01:16:15 +0100

    Create main.c

---
+++ Array/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../../includes/my_puts.h"
+
+int main(int argc, char *argv[])
+{
+  int array[3] = {1, 2, 3};
+  size_t array_size = sizeof(array)/sizeof(array[0]);
+  for(int i = 0; i <= array_size; i++)
+  {
+    printf("%d",array[i]);
+  }
+}
